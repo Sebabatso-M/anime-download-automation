3198ae67ddd440e2033632b430ef5ed82d7d1bf4
Sebabatso Mashego <52962008+Sebabatso-M@users.noreply.github.com>
2021-03-05 03:40:53 +0200

    Add files via upload

---
+++ download.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+const {
+    readFileSync
+} = require('fs');
+
+async function download(site) {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = await browser.newPage();
+
+    try {
+
+        await page.goto(site);
+        
+        let link = await page.$eval('.dowloads a', ele => ele.href);
+
+        await page.goto(link);
+
+        await page.$$eval('div.mirror_link:nth-child(5) > .dowloads', arr => arr.map(i => i.firstChild.href));
+
+
+    } catch (er) {
+        console.log(er);
+    } finally {
+        setTimeout(() => {
+            browser.close();
+        }, 3000);
+
+    }
+}
+
+function clickDownloadLink() {
+
+}
+
+download("https://www2.gogoanime.sh/diamond-ace-episode-1");
